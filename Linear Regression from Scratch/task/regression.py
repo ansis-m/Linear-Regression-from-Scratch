edc3332b93a25c69c8d8ba5fa9d613349356d555
ansis@home <ansis@home>
2023-09-26 11:15:31 +0300

    stage3 completed

--- Linear Regression from Scratch/task/regression.py
+++ Linear Regression from Scratch/task/regression.py
@@ -1,10 +1,9 @@
 
 import numpy as np
 
-x = [4, 4.5, 5, 5.5, 6, 6.5, 7]
-w = [1, -3, 2, 5, 0, 3, 6]
-z = [11, 15, 12, 9, 18, 13, 16]
-y = [33, 42, 45, 51, 53, 61, 62]
+capacity = [0.9, 0.5, 1.75, 2.0, 1.4, 1.5, 3.0, 1.1, 2.6, 1.9]
+age = [11, 11, 9, 8, 7, 7, 6, 5, 5, 4]
+cost_per_ton = [21.95, 27.18, 16.9, 15.37, 16.03, 18.15, 14.22, 18.72, 15.4, 14.69]
 
 
 class CustomLinearRegression:
@@ -13,17 +12,26 @@ class CustomLinearRegression:
 
         self.fit_intercept = fit_intercept
         self.coefficients = ...
-        self.y_pred = ...
+        self.cost_per_ton_pred = ...
+        self.map = {}
 
     def fit(self):
 
-        data = np.column_stack([x, w, z])
-        self.coefficients, _, _, _ = np.linalg.lstsq(data, y, rcond=None)
-        self.y_pred = np.dot(data, self.coefficients)
+        data = np.column_stack([np.ones(len(capacity)), capacity, age])
+        self.coefficients, _, _, _ = np.linalg.lstsq(data, cost_per_ton, rcond=None)
+        self.cost_per_ton_pred = np.dot(data, self.coefficients)
+
+        sst = sum((np.array(cost_per_ton) - np.mean(cost_per_ton))**2)
+        sse = np.sum((self.cost_per_ton_pred - np.array(cost_per_ton))**2)
+
+        self.map['Intercept'] = self.coefficients[0]
+        self.map['Coefficient'] = self.coefficients[1:]
+        self.map['R2'] = 1 - sse/sst
+        self.map['RMSE'] = np.sqrt(sse/len(cost_per_ton))
         return self
 
     def print(self):
-        print(self.y_pred)
+        print(self.map)
 
 
 def main():
--- Linear Regression from Scratch/task/test/tests.py
+++ Linear Regression from Scratch/task/test/tests.py
@@ -1,14 +1,10 @@
 import re
 
-import numpy as np
 from hstest import StageTest, CheckResult, dynamic_test, TestedProgram
 
 
 def get_number(string):
     return list(map(float, re.findall(r'-*\d*\.\d+|-*\d+', string)))
-
-
-answer = np.array([35.35993478, 41.13594995, 45.19749381, 51.16062856, 52.99980512, 60.91799349, 61.01596736])
 
 
 class LinearRegression(StageTest):
@@ -21,20 +17,72 @@ class LinearRegression(StageTest):
         if len(reply) == 0:
             return CheckResult.wrong("No output was printed. Print output in the right format.")
 
-        if '[' not in reply or ']' not in reply or ',' in reply:
-            return CheckResult.wrong("Print output as numpy array")
+        if '{' not in reply or '}' not in reply or ":" not in reply or "," not in reply:
+            return CheckResult.wrong("Print output in dictionary format")
 
-        reply = get_number(reply)
+        if 'array' not in reply:
+            return CheckResult.wrong("Return coefficient(s) in numpy array")
 
-        if len(reply) != 7:
-            return CheckResult.wrong(f"y should contain 7 values, found {len(reply)}")
+        if reply.count(',') != 4 or reply.count(':') != 4 or reply.count('}') != reply.count('{'):
+            return CheckResult.wrong('The dictionary output is not properly constructed.')
 
-        for reply_coef, answer_coef in zip(reply, answer):
-            # 2% error is allowed
-            error = answer_coef * 0.02
-            if not answer_coef - error < reply_coef < answer_coef + error:
-                return CheckResult.wrong(
-                    f"Incorrect y array. Check your fit_intercept=False and predict method implementations")
+        output = reply.replace("{", "").replace("}", "").lower().split(", '")
+
+        if len(output) != 4:
+            return CheckResult.wrong(f"Number of items in dictionary should be 4, {len(output)} present")
+
+        output = [j.replace("'", "") for j in output]
+        output1, output2, output3, output4 = output
+
+        name1, answer1 = output1.strip().split(':')
+        name2, answer2 = output2.strip().split(':')
+        name3, answer3 = output3.strip().split(':')
+        name4, answer4 = output4.strip().split(':')
+
+        answers = {
+            name1.strip(): answer1, name2.strip(): answer2,
+            name3.strip(): answer3, name4.strip(): answer4}
+
+        intercept = answers.get('intercept', '0000000')
+        coefficient = answers.get('coefficient', '0000000')
+        coefficient = re.sub('array', '', coefficient)
+        r2 = answers.get('r2', '0000000')
+        rmse = answers.get('rmse', '0000000')
+
+        if intercept == '0000000' or coefficient == '0000000' or len(intercept) == 0 or len(coefficient) == 0:
+            return CheckResult.wrong("Print values for both Intercept and Coefficient")
+
+        if r2 == '0000000' or rmse == '0000000' or len(r2) == 0 or len(rmse) == 0:
+            return CheckResult.wrong("Print values for both R2 and RMSE")
+
+        intercept = get_number(intercept)
+        if len(intercept) != 1:
+            return CheckResult.wrong(f"Intercept should contain a single value, found {len(intercept)}")
+        intercept = intercept[0]
+        if not 18.6 < intercept < 18.8:
+            return CheckResult.wrong("Wrong value for Intercept")
+
+        coefficient = get_number(coefficient)
+        if len(coefficient) != 2:
+            return CheckResult.wrong(f"Coefficient should contain two values, found {len(coefficient)}")
+        if not -3.3 < coefficient[0] < -3.1:
+            return CheckResult.wrong("Wrong value for beta1")
+        if not 0.5 < coefficient[1] < 0.7:
+            return CheckResult.wrong("Wrong value for beta2")
+
+        r2 = get_number(r2)
+        if len(r2) != 1:
+            return CheckResult.wrong(f"R2 should contain a single value, found {len(r2)}")
+        r2 = r2[0]
+        if not 0.7 < r2 < 0.9:
+            return CheckResult.wrong("Wrong value for R2 score")
+
+        rmse = get_number(rmse)
+        if len(rmse) != 1:
+            return CheckResult.wrong(f"RMSE should contain a single value, found {len(rmse)}")
+        rmse = rmse[0]
+        if not 1.6 < rmse < 1.8:
+            return CheckResult.wrong("Wrong value for RMSE score. Did you take the square root of mean squared error?")
 
         return CheckResult.correct()
 
